e377bfbd156890f52ee57e43d78edc13c0248d40
Femi Sodiya <femisd@gmail.com>
2020-02-21 22:10:33 +0000

    collision detection and bootleg animation animation system (needs css fix).

--- client/src/Components/Connector/Connector.jsx
+++ client/src/Components/Connector/Connector.jsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import io from 'socket.io-client';
 import carImg from "../../assets/images.jpg"
+import npcCar from "../../assets/npc-car.png"
 import "./Connector.css"
 import '../../App.css';
-import {LeftRoadSide, RightRoadSide} from "../Road/RoadSide";
+import "../Road/RoadAnimations.css"
+import {LeftRoadSide, RightRoadSide, sideAnimationController} from "../Road/RoadSide";
 import ReactDOM from "react-dom";
 
 
@@ -13,6 +15,7 @@ export class Connector extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             dataMessage: {
                 horizontalPosition: 0,
@@ -27,8 +30,8 @@ export class Connector extends Component {
     }
 
     componentDidMount() {
-        console.log("component mounted" + this.state.dataMessage);
-        this.socket = io.connect("http://localhost:" + port, { // TODO: can be changed to IPv4 during demo.
+        let uri = "http://10.77.86.173:" + port; // TODO: should be changed to IPv4 during demo.
+        this.socket = io.connect(uri, {
             reconnectionDelay: 1000,
             reconnection: true,
             reconnectionAttempts: 10,
@@ -37,7 +40,7 @@ export class Connector extends Component {
             upgrade: false,
             rejectUnauthorized: false
         });
-        console.log("connecting to server on :",); // Add heroku URL
+        console.log("connecting to server on :", uri); // Add heroku URL
         this.socket.on("message", data => {
             this.setState({dataMessage: data});
         });
@@ -46,8 +49,6 @@ export class Connector extends Component {
     submitData(data) {
         this.setState({dataMessage: data});
         this.socket.json.emit('message', data);
-        console.log("sent message " + data);
-        console.log("current message " + this.state.dataMessage)
     }
 
     clutchPress() {
@@ -114,25 +115,25 @@ export class Connector extends Component {
         let carImagePosition;
         switch (this.state.dataMessage.horizontalPosition) {
             case 3:
-                carImagePosition = "carImage-right-3";
+                carImagePosition = "carImage-right-3 playerCar";
                 break;
             case 2:
-                carImagePosition = "carImage-right-2";
+                carImagePosition = "carImage-right-2 playerCar";
                 break;
             case 1:
-                carImagePosition = "carImage-right-1";
+                carImagePosition = "carImage-right-1 playerCar";
                 break;
             case -1:
-                carImagePosition = "carImage-left-1";
+                carImagePosition = "carImage-left-1 playerCar";
                 break;
             case -2:
-                carImagePosition = "carImage-left-2";
+                carImagePosition = "carImage-left-2 playerCar";
                 break;
             case -3:
-                carImagePosition = "carImage-left-3";
+                carImagePosition = "carImage-left-3 playerCar";
                 break;
             default:
-                carImagePosition = "carImage-middle";
+                carImagePosition = "carImage-middle playerCar";
 
 
         }
@@ -197,7 +198,10 @@ export class TestCarAndControls extends Component {
     constructor(props) {
         super(props);
         this.playerRef = React.createRef();
-        this.state = {gameTimer: 0}
+        this.npcRef1 = React.createRef();
+        this.npcRef2 = React.createRef();
+        this.npcRef3 = React.createRef();
+        this.state = {gameTimer: 0, collision: false}
     }
 
 
@@ -207,13 +211,28 @@ export class TestCarAndControls extends Component {
             this.setState(({gameTimer}) => ({
                 gameTimer: gameTimer + 1
             }))
-        }, 1000)
+        }, 300)
 
 
     }
 
-    componentDidUpdate() {
-        this.positionBindingHandler(this.playerRef.current);
+    componentDidUpdate(prevState) {
+        let playerPosition = this.positionBindingHandler(this.playerRef.current);
+        let npc1Position = this.positionBindingHandler(this.npcRef1.current);
+        let npc2Position = this.positionBindingHandler(this.npcRef2.current);
+        let npc3Position = this.positionBindingHandler(this.npcRef3.current);
+
+        // console.log("previous state: ", prevState);
+
+        if (!this.state.collision) {
+            this.collisionController(
+                this.positionBindingHandler(this.playerRef.current),
+                this.positionBindingHandler(this.npcRef1.current),
+                this.positionBindingHandler(this.npcRef2.current),
+                this.positionBindingHandler(this.npcRef3.current));
+        }
+        console.log("collision: ", this.state.collision);
+
     }
 
     componentWillUnmount() {
@@ -221,10 +240,51 @@ export class TestCarAndControls extends Component {
     }
 
     positionBindingHandler = (ref) => {
-        let elementPosition = ReactDOM.findDOMNode(ref).getBoundingClientRect(); // use this.ref.current
-        console.log("Player position is: ", elementPosition);
-        return elementPosition;
+        return ReactDOM.findDOMNode(ref).getBoundingClientRect();
     };
+
+    collisionHandler(playerPosition, npcPosition,) {
+
+        const positionVariant = 90; // A relative value for error because detection may not be pixel perfect.
+
+        let playerX = playerPosition.x;
+        let playerY = playerPosition.y;
+
+        let npcX = npcPosition.x;
+        let npcY = npcPosition.y;
+
+        return Math.abs(playerX - npcX) < positionVariant && (Math.abs(playerY - npcY) < positionVariant);
+
+    }
+
+    collisionController(playerPosition, npc1Position, npc2Position, npc3Position) {
+        if (
+            this.collisionHandler(
+                playerPosition,
+                npc1Position
+            )) {
+            this.setState({collision: true});
+            console.log("collision detected at lane 1")
+        }
+        if (
+            this.collisionHandler(
+                playerPosition,
+                npc2Position
+            )) {
+            this.setState({collision: true});
+            console.log("collision detected lane 2")
+        }
+        if (
+            this.collisionHandler(
+                playerPosition,
+                npc3Position
+            )) {
+            this.setState({collision: true});
+            console.log("collision detected lane 3")
+
+        }
+
+    }
 
 
     render() {
@@ -232,6 +292,11 @@ export class TestCarAndControls extends Component {
         let speed = this.props.speed;
         let clutch = this.props.clutch;
         let horizontalPosition = this.props.horizontalPosition;
+
+        let npc1Class = `carImage-left-3 npcCarImage${npcAnimationHandler(speed)}`;
+        let npc2Class = `carImage-middle npcCarImage${npcAnimationHandler(speed)}`;
+        let npc3Class = `carImage-right-3 npcCarImage${npcAnimationHandler(speed)}`;
+
         return (
             <div>
 
@@ -251,11 +316,95 @@ export class TestCarAndControls extends Component {
                     </div>
 
 
-                    <img className={carImagePosition} ref={this.playerRef} src={carImg} alt={"car"}/>
+                    <div className="carDiv">
 
+                        <img className={carImagePosition} ref={this.playerRef} src={carImg} alt={"car"}/>
+                        <img className={npc1Class} ref={this.npcRef1} src={npcCar} alt={"npcCar1"}/>
+                        <img className={npc2Class} ref={this.npcRef2} src={npcCar} alt={"npcCar2"}/>
+                        <img className={npc3Class} ref={this.npcRef3} src={npcCar} alt={"npcCar3"}/>
+                    </div>
                 </div>
             </div>
 
         );
     }
 }
+
+const npcAnimationHandler = (speed) => {
+
+    let npcAnimation;
+
+
+    switch (true) {
+
+        case (speed <= 0):
+            npcAnimation = "";
+            break;
+
+        case (1 < speed && speed < 19):
+            npcAnimation = " npc-animation-speed-1";
+            break;
+        case (19 < speed && speed < 29):
+
+            npcAnimation = " npc-animation-speed-2";
+            break;
+        case (29 < speed && speed < 39):
+
+            npcAnimation = " npc-animation-speed-3";
+            break;
+        case (30 < speed && speed < 49):
+
+            npcAnimation = " npc-animation-speed-4";
+            break;
+        case (49 < speed && speed < 59):
+
+            npcAnimation = " npc-animation-speed-5";
+            break;
+        case (59 < speed && speed < 69):
+
+            npcAnimation = " npc-animation-speed-6";
+            break;
+
+        case (69 < speed && speed < 79):
+
+            npcAnimation = " npc-animation-speed-7";
+            break;
+
+        case (79 < speed):
+
+            npcAnimation = " npc-animation-speed-7";
+            break;
+
+
+    }
+
+    return npcAnimation
+
+};
+
+// const npcRandomizerController = (animationHandler, speed) => {
+//     const baseDuration = 5.5;
+//     let speedModifier = 1;
+//     if (speed < 90) {
+//         speedModifier = speed / 10
+//     }
+//
+//     setTimeout(() => {
+//             while (true) {
+//                 let isRendering = Math.random() >= 0.5;
+//
+//                 if (isRendering) {
+//                     console.log("Rendere"isRendering);
+//                     return animationHandler;
+//                 }
+//
+//             }
+//
+//
+//         },
+//         baseDuration / speedModifier
+//     );
+//     return "";
+//
+//
+// }
--- client/src/Components/Road/RoadSide.jsx
+++ client/src/Components/Road/RoadSide.jsx
@@ -32,9 +32,7 @@ export class LeftRoadSide extends Component {
 
 
     positionBindingHandler = (ref) => {
-        let elementPosition = ReactDOM.findDOMNode(ref).getBoundingClientRect(); // use this.ref
-        console.log("Tree position is: ", elementPosition);
-        return elementPosition;
+        return ReactDOM.findDOMNode(ref).getBoundingClientRect();
     };
 
 
@@ -79,49 +77,33 @@ const sideAnimationController = (speed) => {
     switch (true) {
 
         case (speed <= 0):
-            console.log("stopped at speed: ", speed);
             sideAnimation = "sideImg";
             break;
 
         case (1 < speed && speed < 19):
-            console.log("speed animation 1 at speed: ", speed);
             sideAnimation = "sideImg side-animation-speed-1";
             break;
         case (19 < speed && speed < 29):
-            console.log("speed animation 2 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-2";
             break;
         case (29 < speed && speed < 39):
-            console.log("speed animation 3 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-3";
             break;
         case (30 < speed && speed < 49):
-            console.log("speed animation 4 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-4";
             break;
         case (49 < speed && speed < 59):
-            console.log("speed animation 5 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-5";
             break;
         case (59 < speed && speed < 69):
-            console.log("speed animation 6 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-6";
             break;
 
         case (69 < speed && speed < 79):
-            console.log("speed animation 7 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-7";
             break;
 
         case (79 < speed):
-            console.log("speed animation 7 at speed: ", speed);
-
             sideAnimation = "sideImg side-animation-speed-7";
             break;
 
